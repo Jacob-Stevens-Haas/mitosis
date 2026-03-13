c2116c94146a99938480c825f207a47296f503d2
Jacob Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2026-03-13 00:01:08 -0700

    TYP: Clarify ExpResult type and bump python (#66)

--- mitosis/tests/mock_part2.py
+++ mitosis/tests/mock_part2.py
@@ -5,16 +5,16 @@ import numpy as np
 from numpy.typing import NBitBase
 from numpy.typing import NDArray
 
-from mitosis._typing import ExpResults
+from mitosis.typing import ExpResult
 
 
 def fit_and_score(
     data: NDArray[np.floating[NBitBase]], metric: Literal["len"] | Literal["zero"]
-) -> ExpResults:
+) -> ExpResult:
     if metric == "len":
-        return {"main": len(data)}
+        return {"main": len(data), "metrics": {}, "data": None}
     elif metric == "zero":
-        return {"main": 0}
+        return {"main": 0, "metrics": {}, "data": None}
 
 
 def bad_runnable(*args: Any, **kwargs: Any) -> int:
